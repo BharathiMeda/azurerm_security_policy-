1f1595cf0ff9cb802a74bf1b3576d4c43497d33a
BharathiMeda <128385999+BharathiMeda@users.noreply.github.com>
2025-04-22 11:59:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
+data "azurerm_virtual_network" "Test" {
+  name                = "production"
+  resource_group_name = "networking"
+}
 # Create a Policy Definition
+
 resource "azurerm_policy_definition" "deny_public_ip" {
   name         = "Deny-Public-IP"
   policy_type  = "Custom"
@@ -25,12 +30,12 @@ POLICY_RULE
 METADATA
 }
 
-# Assign the Policy to a Scope (e.g., a Resource Group)
-resource "azurerm_policy_assignment" "deny_public_ip_assignment" {
+resource "azurerm_resource_policy_assignment" "deny_public_ip_assignment" {
   name                 = "Deny-Public-IP-Assignment"
-  scope                = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group_name}"
+  resource_id       =    data.azurerm_virtual_network.Test.id
+  #scope                = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group_name}"
   policy_definition_id = azurerm_policy_definition.deny_public_ip.id
   display_name         = var.policy_assignment_display_name
   description          = "This assignment denies the creation of public IPs in the specified resource group."
-  enforcement_mode     = var.policy_enforcement_mode
+  #enforcement_mode     = var.policy_enforcement_mode
 }
